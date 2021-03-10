058d95820e067de28290f4162f2b3cfc8ff08d9f
sean swe <seanswe@gmail.com>
2021-03-09 22:39:42 -0800

    added dev dependencies to pipfile

--- oasis.py
+++ oasis.py
@@ -12,14 +12,14 @@ def get_lmp_zip(year, month, market="DAM", node="DLAP_SCE-APND"):
 
     fmt = "%Y%m%dT%H:%M-0000"
 
+    pacific = pytz.timezone("US/Pacific")
+
     url = "http://oasis.caiso.com/oasisapi/SingleZip?"
 
     start = datetime(year, month, 1)
     end = start + relativedelta(months=1)
-    start = pacific.localize(start).astimezone(utc).strftime(fmt)
-    end = pacific.localize(end).astimezone(utc).strftime(fmt)
-    # start = start + "-0000"
-    # end = end + "-0000"
+    start = pacific.localize(start).astimezone(pytz.UTC).strftime(fmt)
+    end = pacific.localize(end).astimezone(pytz.UTC).strftime(fmt)
     market_dict = {"DAM": "PRC_LMP", "RTM": "PRC_INTVL_LMP", "RTPD": "PRC_RTPD_LMP"}
     params = {
         "market_run_id": market,
@@ -33,10 +33,15 @@ def get_lmp_zip(year, month, market="DAM", node="DLAP_SCE-APND"):
 
     str_params = "&".join("%s=%s" % (k, v) for k, v in params.items())
 
-    return str_params
+    r = requests.get(url, params=str_params)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    csv = z.open(z.namelist()[0])
+    return pd.read_csv(csv, parse_dates=[2])
 
 
-print(get_lmp_zip(2020, 10))
+df = get_lmp_zip(2021, 1)
+
+print(df.head())
 
 
 # for m in range(1, 3):
